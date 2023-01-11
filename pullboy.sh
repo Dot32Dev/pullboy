bc6997055c304910bc2fa7c09cc370c370e7c9ef
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-01-11 18:25:49 +0800

    git pull when outdated

--- pullboy.sh
+++ pullboy.sh
@@ -1,17 +1,17 @@
 #!/bin/sh  
 while true  
 do  
-	# Create a variable to store whether the current branch is up to date with its remote branch
-	# If it is, the variable will be set to false, ottherwise it will be true
-	# out_of_date=$(git rev-parse HEAD != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
-	# sed 's/\// /g') | cut -f1))
-
-	# echo $out_of_date
-	
+	echo "Checking for updates..."
 
 	# Check if the current branch is up to date with its remote branch
 	[ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
-	sed 's/\// /g') | cut -f1) ] && echo "Up to date" || echo "Outdated"
+	sed 's/\// /g') | cut -f1) ] && out_of_date=false || echo out_of_date=true
+
+	# If the current branch is not up to date with its remote branch, pull the latest changes
+	if [ $out_of_date = true ]; then
+		echo "Pulling latest changes"
+		git pull
+	fi
 	
 	# Wait a minute
 	sleep 60  

52a2681aa2a18cf952f68574f190a26158b32417
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-01-11 18:10:37 +0800

    Test

--- pullboy.sh
+++ pullboy.sh
@@ -3,16 +3,16 @@ while true
 do  
 	# Create a variable to store whether the current branch is up to date with its remote branch
 	# If it is, the variable will be set to false, ottherwise it will be true
-	out_of_date=$(git rev-parse HEAD != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
-	sed 's/\// /g') | cut -f1))
+	# out_of_date=$(git rev-parse HEAD != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+	# sed 's/\// /g') | cut -f1))
 
-	echo $out_of_date
+	# echo $out_of_date
 	
 
 	# Check if the current branch is up to date with its remote branch
 	# If not, pull the remote branch
-	# [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
-	# sed 's/\// /g') | cut -f1) ] && echo "-" || git pull
+	[ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+	sed 's/\// /g') | cut -f1) ] && echo "Up to date" || echo "Outdated"
 	
 	# Wait a minute
 	sleep 60  
